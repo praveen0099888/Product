4379cf1f8aaea37d1db6b5167fe4ca2b2bcef7e6
Praveen_372 <praveenchoudhary372@gmail.com>
2023-05-23 16:02:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowallComponent } from './showall/showall.component';
 
-const routes: Routes = [];
+import { ShowexpiredComponent } from './showexpired/showexpired.component';
+
+const routes: Routes = [ {
+  path:'all', component: ShowallComponent
+},{
+  path:'exp', component: ShowexpiredComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../node_modules/ng-zorro-antd/src/ng-zorro-antd.min.css','./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private route:Router){}
+  ngOnInit(): void {
+    this.route.navigateByUrl('/all');
+  }
   title = 'productfrontend';
 }
